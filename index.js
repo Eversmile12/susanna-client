4dfb66af2ef13c3b1835e510bfd8f67867bb25a5
Vittorio Rivabella <20019426@uhi.ac.uk>
2021-03-20 16:10:40 +0100

    sorting too fast processing

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
     });
 
     $(".main-header").mouseenter(mixHeader).mouseleave(restoreHeader);
-    console.log(eraserRadius);
+
     // Eraser picker change listener -> could be definitely handled in a better way....
     // $("#eraser").click(() => {
     //     isErasing = !isErasing;
@@ -53,9 +53,11 @@ $(document).ready(function () {
         };
     }
 
+    var start = new Date();
+    var end;
+
     // When mouseDown event gets triggered start capturing movements
     function onClickDown(e) {
-        console.log("mouse down");
         // Start panting
         isPainting = true;
         //Storing mouse position at current time
@@ -65,7 +67,15 @@ $(document).ready(function () {
     //When mouseup event is triggered: stop drawing, draw image on #main canvas and clear #draft .
     function onClickUp() {
         if (isAuto) {
-            processDrawing();
+            end = new Date();
+            console.log("You're calling narciso 🤖");
+
+            if(end - start > 2000){
+                processDrawing();
+                start = end;
+            }else{
+                alert("Slow down, not yet quantum baybe🤖")
+            }
         } 
 
         isPainting = false;
@@ -82,7 +92,6 @@ $(document).ready(function () {
         ctx.lineWidth = 2;
         // If it's erasing set color to white
         if (isErasing && isPainting) {
-            console.log(eraserRadius);
             ctx.strokeStyle = "#fff";
             ctx.lineWidth = eraserRadius;
         }
@@ -120,12 +129,11 @@ $(document).ready(function () {
     });
 
     $("#process-drawing").click(() => {
-        console.log("Process drawing clicked");
         processDrawing();
     });
 
     function processDrawing() {
-        console.log("php called");
+
         let dataURL = canvas.toDataURL("img/jpeg");
         let image = UtoB(dataURL, "img/jpeg");
         let data = new FormData();
@@ -165,7 +173,6 @@ function restoreHeader(){
 function fetchAndSetRandImage(){
      let images = "output0_fake_B.png output1_fake_B.png output10_fake_B.png output100_fake_B.png output101_fake_B.png output102_fake_B.png output103_fake_B.png output104_fake_B.png output105_fake_B.png output106_fake_B.png output107_fake_B.png output108_fake_B.png output109_fake_B.png output11_fake_B.png output110_fake_B.png output111_fake_B.png output112_fake_B.png output113_fake_B.png output114_fake_B.png output115_fake_B.png output116_fake_B.png output117_fake_B.png output118_fake_B.png output119_fake_B.png output12_fake_B.png output120_fake_B.png output121_fake_B.png output122_fake_B.png output123_fake_B.png output124_fake_B.png output125_fake_B.png output126_fake_B.png output127_fake_B.png output128_fake_B.png output129_fake_B.png output13_fake_B.png output130_fake_B.png output131_fake_B.png output132_fake_B.png output133_fake_B.png output134_fake_B.png output135_fake_B.png output136_fake_B.png output137_fake_B.png output138_fake_B.png output139_fake_B.png output14_fake_B.png output140_fake_B.png output141_fake_B.png output142_fake_B.png output143_fake_B.png output144_fake_B.png output145_fake_B.png output146_fake_B.png output147_fake_B.png output148_fake_B.png output149_fake_B.png output15_fake_B.png output150_fake_B.png output151_fake_B.png output152_fake_B.png output153_fake_B.png output154_fake_B.png output155_fake_B.png output156_fake_B.png output157_fake_B.png output158_fake_B.png output159_fake_B.png output16_fake_B.png output160_fake_B.png output161_fake_B.png output162_fake_B.png output163_fake_B.png output164_fake_B.png output165_fake_B.png output166_fake_B.png output167_fake_B.png output168_fake_B.png output169_fake_B.png output17_fake_B.png output170_fake_B.png output171_fake_B.png output172_fake_B.png output173_fake_B.png output174_fake_B.png output175_fake_B.png output176_fake_B.png output177_fake_B.png output178_fake_B.png output179_fake_B.png output18_fake_B.png output180_fake_B.png output181_fake_B.png output182_fake_B.png output183_fake_B.png output184_fake_B.png output185_fake_B.png output186_fake_B.png output187_fake_B.png output188_fake_B.png output19_fake_B.png output2_fake_B.png output20_fake_B.png output21_fake_B.png output22_fake_B.png output23_fake_B.png output24_fake_B.png output25_fake_B.png output26_fake_B.png output27_fake_B.png output28_fake_B.png output29_fake_B.png output3_fake_B.png output30_fake_B.png output31_fake_B.png output32_fake_B.png output33_fake_B.png output34_fake_B.png output35_fake_B.png output36_fake_B.png output37_fake_B.png output38_fake_B.png output39_fake_B.png output4_fake_B.png output40_fake_B.png output41_fake_B.png output42_fake_B.png output43_fake_B.png output44_fake_B.png output45_fake_B.png output46_fake_B.png output47_fake_B.png output48_fake_B.png output49_fake_B.png output5_fake_B.png output50_fake_B.png output51_fake_B.png output52_fake_B.png output53_fake_B.png output54_fake_B.png output55_fake_B.png output56_fake_B.png output57_fake_B.png output58_fake_B.png output59_fake_B.png output6_fake_B.png output60_fake_B.png output61_fake_B.png output62_fake_B.png output63_fake_B.png output64_fake_B.png output65_fake_B.png output66_fake_B.png output67_fake_B.png output68_fake_B.png output69_fake_B.png output7_fake_B.png output70_fake_B.png output71_fake_B.png output72_fake_B.png output73_fake_B.png output74_fake_B.png output75_fake_B.png output76_fake_B.png output77_fake_B.png output78_fake_B.png output79_fake_B.png output8_fake_B.png output80_fake_B.png output81_fake_B.png output82_fake_B.png output83_fake_B.png output84_fake_B.png output85_fake_B.png output86_fake_B.png output87_fake_B.png output88_fake_B.png output89_fake_B.png output9_fake_B.png output90_fake_B.png output91_fake_B.png output92_fake_B.png output93_fake_B.png output94_fake_B.png output95_fake_B.png output96_fake_B.png output97_fake_B.png output98_fake_B.png output99_fake_B.png printFiles.sh";
     images = images.split(" ");
-    console.log(images)
     var imageIndex = Math.round(Math.random()*(images.length-1));
 
     $("#result-image").attr("src", "./graphics-assets/images/"+images[imageIndex]);
